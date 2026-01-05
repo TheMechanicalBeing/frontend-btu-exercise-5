d7e6bcdeaead5e591e28e5f955f354e4d4fb0973
TheMechanicalBeing <tornike.tsulukidze@gmail.com>
2026-01-05 22:37:15 +0400

    Add fetch logic to extract user data after pressing button

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("Hello World!");
+const userButton = document.getElementById("userButton");
+
+userButton.addEventListener("click", async (e) => {
+    try{
+        const response = await fetch('https://jsonplaceholder.typicode.com/users');
+        const data = await response.json();
+
+        const cardsContainer = document.createElement("div");
+
+        data.forEach(user => {
+            const card = document.createElement('div');
+            card.innerHTML = `
+                <h4>User ${user.id} info</h4>
+                <ul>
+                    <li>Name - ${user.name}</li>
+                    <li>Username - ${user.username}</li>
+                    <li>Email - ${user.email}</li>
+                    <li>City - ${user.address.city}</li>
+                    <li>Phone - ${user.phone}</li>
+                    <li>Website - ${user.website}</li>
+                    <li>Company - ${user.company.name}</li>
+                </ul>`
+            cardsContainer.appendChild(card);
+        })
+
+        document.querySelector("body").appendChild(cardsContainer);
+    } catch(error) {
+        console.error(error);
+    }
+})
